6956a68c749c92d66cdad7f5b23908f94d86d047
Andersen Fan <as181920@gmail.com>
2017-07-15 20:42:05 +0800

    Add messges from chat page

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,5 +6,8 @@ class MessagesController < ApplicationController
   end
 
   def from
+    @user = User.find_by(wxuin: params[:wxuin])
+    @q = @user.messages.where(from: params[:from]).ransack(params[:q])
+    @messages = @q.result.order("id desc").page(params[:page]).per(params[:per] || 10)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "messages/from/:user_name", to: "messages#from", as: :messages_from_user
   get "messages/for/:wxuin", to: "messages#for", as: :messages_for_user
+  get "messages/for/:wxuin/from/:from", to: "messages#from", as: :messages_from_chat
 end
